7df9f049eedecc8ff8955ff24fe2538cdc4176fb
Alexemak <aliaksei.makuch@cogniteq.com>
2020-01-31 18:09:27 +0300

    fixed image error

--- src/resources/images/index.js
+++ src/resources/images/index.js
@@ -6,7 +6,6 @@ import auth_usb from './auth_usb.png'
 import home_news_1 from './home_news_1.png'
 import home_news_2 from './home_news_2.png'
 import single_news from './single-news.png'
-import single_news_image from './single-news'
 
 export default {
   home_about,
@@ -16,6 +15,5 @@ export default {
   auth_usb,
   home_news_1,
   home_news_2,
-  single_news,
-  single_news_image
+  single_news
 }

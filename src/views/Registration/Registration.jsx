ea3dd9544ab167564da3253d58cf03bf7bd2ecda
Alexemak <aliaksei.makuch@cogniteq.com>
2020-01-31 18:46:31 +0300

    update registr page

--- src/views/Registration/Registration.jsx
+++ src/views/Registration/Registration.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router'
 
 import { Heading } from '../../components'
+
 export default function Registration () {
   const { state } = useLocation()
+  const [activeType, setActiveType] = useState('phone')
 
   const { type } = state
+
+  useEffect(() => {
+    if (type) {
+      setActiveType(type)
+    }
+  }, [type])
+
   return (
     <div>
-      <Heading level={2}>Тип регистрации {type}</Heading>
+      <Heading level={2}>Тип регистрации {activeType}</Heading>
     </div>
   )
 }

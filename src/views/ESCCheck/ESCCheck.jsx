b176300a632e5bdaffad76bcfd6209e80159831c
Alexemak <aliaksei.makuch@cogniteq.com>
2020-03-26 18:25:54 +0300

    start working with esc checking page

--- src/views/ESCCheck/ESCCheck.jsx
+++ src/views/ESCCheck/ESCCheck.jsx
@@ -1,28 +1,34 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+import logger from 'use-reducer-logger'
 
 import { Row, Col } from 'antd'
+import { ESCCheckContext, initialState, reducer } from './context'
 import { DropZone } from './internal'
 import { Container, Heading, Text } from '../../components'
 import { ESCCheckContent } from './styled'
 export default function ESCCheck () {
-  return (
-    <ESCCheckContent>
-      <Container>
-        <Heading brandText='Проверка'>подписи</Heading>
-        <Text>C помощью сервиса «Проверка ЭЦП» можно проверить электронно-цифровые подписи (ЭЦП) в<br />
-          электронном документе. Для проверки ЭЦП используется сертифицированное средство:<br />
-          Программный комплекс криптографической защиты информации LWO Crypt. Версия 1.0
-        </Text>
+  const [state, dispatch] = useReducer(logger(reducer), initialState)
 
-        <Row gutter={[24, 24]}>
-          <Col md={12}>
-            <DropZone />
-          </Col>
-          <Col md={12}>
-            <DropZone />
-          </Col>
-        </Row>
-      </Container>
-    </ESCCheckContent>
+  const { types } = state
+  return (
+    <ESCCheckContext.Provider value={{ state, dispatch }}>
+      <ESCCheckContent>
+        <Container>
+          <Heading brandText='Проверка'>подписи</Heading>
+          <Text>C помощью сервиса «Проверка ЭЦП» можно проверить электронно-цифровые подписи (ЭЦП) в<br />
+            электронном документе. Для проверки ЭЦП используется сертифицированное средство:<br />
+            Программный комплекс криптографической защиты информации LWO Crypt. Версия 1.0
+          </Text>
+
+          <Row gutter={[24, 24]}>
+            {types.map((type, idx) => (
+              <Col md={12} key={idx}>
+                <DropZone type={type} />
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </ESCCheckContent>
+    </ESCCheckContext.Provider>
   )
 }
--- src/views/ESCCheck/internal/DropZone/DropZone.jsx
+++ src/views/ESCCheck/internal/DropZone/DropZone.jsx
@@ -1,16 +1,32 @@
-import React, { useState } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import classNames from 'classnames'
+import { useCountUp } from 'react-countup'
 
+import LoadStatus from './styled'
+import { ESCCheckContext } from '../../context'
 import { Text, Button } from '../../../../components'
 import './DropZone.scss'
 
-export default function () {
+export default function ({ type }) {
   const [status, setStatus] = useState(false)
-  const [isLoaded, setLoaded] = useState(false)
+  const [isLoading, setLoading] = useState(false)
+  const { countUp, start } = useCountUp({
+    start: 0,
+    end: 100,
+    duration: Math.floor(1 + Math.random() * (5))
+  })
+  const { state, dispatch } = useContext(ESCCheckContext)
+
+  useEffect(() => {
+    if (countUp === 100) {
+      setLoading(false)
+    }
+  }, [countUp])
 
   const dropzoneClasses = classNames({
     dropzone: true,
-    'dropzone--status-drag': status
+    'dropzone--dragged': status,
+    'dropzone--loading': isLoading
   })
 
   const onDragEnter = e => {
@@ -25,17 +41,15 @@ export default function () {
   }
 
   const onDrop = e => {
-    console.log(e)
     setStatus(false)
-    if (!status) {
-      setLoaded(true)
-    }
-    const file = e.dataTransfer.files[0]
-    console.log(file)
+    setLoading(true)
+    start()
+    dispatch({ type: 'GET_FILE', payload: { type, file: e.dataTransfer.files[0] } })
     e.preventDefault()
   }
 
   const doNothing = e => e.preventDefault()
+
   return (
     <div
       data-text='Бросьте файл сюда'
@@ -45,9 +59,17 @@ export default function () {
       onDragLeave={onDragLeave}
       onDrop={onDrop}
     >
-      <Text bolder>Перетяните подписанный документ (.pdf/.doc/.zip файл)</Text>
-      <Text>или</Text>
-      <Button type='primary'>Выберите файл</Button>
+      {isLoading && <LoadStatus width={countUp} />}
+      {isLoading
+        ? <>
+          <div className='dropzone__text'>Загрузка...</div>
+          <div className='dropzone__text dropzone__text--light'>{state[type].data.name}</div>
+        </>
+        : <>
+          <div className='dropzone__text'>{state[type].defaultText}</div>
+          <div className='dropzone__text dropzone__text--light'>или</div>
+          <Button type='primary'>Выберите файл</Button>
+        </>}
     </div>
   )
 }

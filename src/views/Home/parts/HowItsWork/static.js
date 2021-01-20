9fb33e1256feb306c342c814706dc2072292c133
Hleb Prakhnitski <thane.crios@gmail.com>
2021-01-20 10:36:26 +0300

    updated main videos

--- src/views/Home/parts/HowItsWork/static.js
+++ src/views/Home/parts/HowItsWork/static.js
@@ -3,18 +3,29 @@ import { images } from '../../../../resources'
 const { poster } = images
 const videoData = [
   {
-    text: 'Как отправить документ MTC QuiDox?',
-    url: 'https://www.youtube.com/watch?v=FkpoaLe448I',
+
+    text: 'Как зарегистрироваться в МТС SmartDoc?',
+    url: 'https://youtu.be/M4I2aOYTLJI',
     poster: poster
   },
   {
-    text: 'Как отправить документ MTC QuiDox?',
-    url: 'https://www.youtube.com/watch?v=FkpoaLe448I',
+    text: 'Как подключить МТС ID к МТС SmartDoc?',
+    url: 'https://youtu.be/tCHeDemuN_Y',
     poster: poster
   },
   {
-    text: 'Как отправить документ MTC QuiDox?',
-    url: 'https://www.youtube.com/watch?v=FkpoaLe448I',
+    text: 'Как подлючить USB ключ с ЭЦП ГосСУОК MTS SmartDoc?',
+    url: 'https://youtu.be/PwhnAbPpEc4',
+    poster: poster
+  },
+  {
+    text: 'Пришло пришлашение. Как получить документ?',
+    url: 'https://youtu.be/EFEqBMm8QwA',
+    poster: poster
+  },
+  {
+    text: 'Как создать сообщение и подписать документ МТС ID?',
+    url: 'https://youtu.be/5etTvoUl4_E',
     poster: poster
   }
 ]
